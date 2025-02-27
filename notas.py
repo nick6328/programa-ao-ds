24dfd0f15a6e892db3ecfc35dfd8b6115e6c4347
nick6328 <nicoly.kozar@escola.pr.gov.br>
2025-02-27 11:39:48 -0300

    Create notas.py

---
+++ notas.py
@@ -0,0 +1,12 @@
+nota = 85
+
+if nota >= 90:
+    print("conceito A")
+elif nota >= 80:
+    print("conceito B")
+elif nota >= 70:
+    print("conceito C")
+elif nota >= 60:
+    print("conceito D")
+else:
+    print("conceito F")
