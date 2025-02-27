a299a8d9cd3847dd9421adad934d6592dc09fcd5
nick6328 <nicoly.kozar@escola.pr.gov.br>
2025-02-27 11:44:17 -0300

    Create idade.py

---
+++ idade.py
@@ -0,0 +1,12 @@
+idade = 28
+
+if idade <= 12:
+    print("criança")
+elif idade <= 18:
+    print("adolescente")
+elif idade <= 30:
+    print("jovem adulto")
+elif idade < 60:
+    print("adulto")
+else:
+    print("idoso")
