e4511bb90dbe81094293da9183e55b4433cba490
nick6328 <nicoly.kozar@escola.pr.gov.br>
2025-02-27 11:44:50 -0300

    Create estaçoes.py

---
+++ estaçoes.py
@@ -0,0 +1,10 @@
+mes = "Julho"
+
+if mes in ["Dezembro", "Janeiro", "Fevereiro"]:
+    print("Verão")
+elif mes in ["Março", "Abril", "Maio"]:
+    print("Outono")
+elif mes in ["Junho", "Julho", "Agosto"]:
+    print("Inverno")
+else:
+    print("Primavera")
