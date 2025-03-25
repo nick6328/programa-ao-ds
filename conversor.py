00d218e4d376d562bba9c1fb9b7bdbdc4d3a64bb
nick6328 <nicoly.kozar@escola.pr.gov.br>
2025-03-25 12:03:37 -0300

    Create conversor.py

---
+++ conversor.py
@@ -0,0 +1,9 @@
+# Solicita temperatura em graus Celsius ao usuário
+celsius = float(input("Digite a temperatura em graus Celsius: "))
+
+# Calcula a temperatura em graus Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+
+# Exibe a conversão
+print(f"{celsius} graus Celsius equivalem a {fahrenheit} graus Fahrenheit.")
