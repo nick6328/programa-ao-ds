a5cdc87040a0da8f7251d8abcbd48cefe78bcbd3
nick6328 <nicoly.kozar@escola.pr.gov.br>
2025-03-25 11:50:21 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -5,7 +5,11 @@ num2 = float(input("Digite o segundo número: "))
 # Configura a lógica das operações
 soma = num1 + num2
 subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
 
 # Exibe os resultados
 print(f"Soma: {soma}")
 print(f"Subtração: {subtracao}")
+print(f"multiplicacao: {multiplicacao}")
+print(f"divisao: {divisao}")
