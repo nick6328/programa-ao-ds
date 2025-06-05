2f9c608b9599d74ddfbff5025b2eafe4ec28fa36
nick6328 <nicoly.kozar@escola.pr.gov.br>
2025-06-05 12:01:30 -0300

    Create contagem.py

---
+++ contagem.py
@@ -0,0 +1,8 @@
+# Solicita ao usuário que insira uma frase
+frase = input("Digite uma frase: ")
+
+# Conta o número de caracteres na frase
+num_caracteres = len(frase)
+
+# Exibe o resultado
+print(f"\nA frase possui {num_caracteres} caracteres.")
