8173f289c10b1f5112546df38cbc4032e074b11f
nick6328 <nicoly.kozar@escola.pr.gov.br>
2025-06-27 11:54:04 -0300

    Create conta-caracteres.py

---
+++ conta-caracteres.py
@@ -0,0 +1,5 @@
+# Declara a variável para armazenar string
+texto = "Olá, mundo! Python é divertido." 
+# Aplica função len() na contagem de caracteres
+tamanho = len(texto) 
+print(f"O texto tem {tamanho} caracteres.")
