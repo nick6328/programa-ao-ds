625887539491fc5b7e6af934996f7264e699b081
nick6328 <nicoly.kozar@escola.pr.gov.br>
2025-07-03 12:15:55 -0300

    Create conta-palavras.py

---
+++ conta-palavras.py
@@ -0,0 +1,11 @@
+# Declara a variável com o texto
+
+texto = "Aprender Python é muito divertido e útil."
+
+# Divide a string em palavras e conta
+
+palavras = texto.split()
+
+quantidade = len(palavras)
+
+print(f"O texto tem {quantidade} palavras.")
